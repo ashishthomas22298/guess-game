67452d117f217e63ee2e14da8e1d3d421e3e3e0b
ashishthomas22298 <athomas22298@gmail.com>
2021-01-17 21:06:25 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// function resize() {
+//     let container = $(".container");
+//     let title = $(".row:first-child");
+//     let wrapper = $(".row:last-child");
+
+//     wrapper.outerHeight(container.outerHeight() - title.outerHeight());
+// }
+
+// window.addEventListener('load', (event) => {
+//     resize();
+// });
+
+// window.addEventListener('resize', (event) => {
+//     resize();
+// });
