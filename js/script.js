49281b5e9781ecc5d74edfb86b78c59b1d6c1d79
ashishthomas22298 <athomas22298@gmail.com>
2021-01-17 23:35:54 -0500

    flip animation added

--- js/script.js
+++ js/script.js
@@ -13,3 +13,32 @@
 // window.addEventListener('resize', (event) => {
 //     resize();
 // });
+
+
+function blockClickHandler(event) {
+
+    let innerBlock = $(".block-inner");
+    innerBlock.off();
+
+    let block = $(event.target.parentElement);
+
+    block.children().first().css("transform", "rotateY(90deg)");
+    setTimeout(
+        function() {
+            block.children().last().css("transform", "rotateY(180deg)");
+
+        }, 300);
+
+
+    setTimeout(
+        function() {
+            block.children().last().css("transform", "rotateY(90deg)");
+            setTimeout(
+                function() {
+                    block.children().first().css("transform", "rotateY(0deg)");
+                    innerBlock.on("click", blockClickHandler);
+                }, 300);
+        }, 2000);
+}
+
+$(".block-inner").on("click", blockClickHandler);
